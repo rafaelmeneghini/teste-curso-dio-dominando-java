1c165077e4a5963c45ed8efc55e3f2bef5197feb
Rafael <rafaelmeneghini88@gmail.com>
2023-02-18 23:53:48 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	
+				/*int a = 1;
+		int b = 2;
+		System.out.println("Hello world " + (a+b));*/
+
+	}
+	
+
+}
+
+	class Livros {
+		private String nome;
+		private String npag;
+	}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author rafae
+ *
+ */
+module primeiro_programa_java {
+}
